a647948aee99589e1df5b76b0b919e458a655c46
Jwala Mohith Girisha <jwalamohithgsjce@gmail.com>
2015-01-08 00:45:55 -0800

    Adding counting zeros in sorted array of 1s and 0s

--- Count_0s.cpp
+++ Count_0s.cpp
@@ -13,15 +13,19 @@ void fn_iterative(int arr[], int size);
 void fn_recursive(int arr[], int size);
 int count_zero(int arr[],int position, int size);
 
+void fn_recursivetwo(int arr[], int size);
+int count_zerotwo(int arr[],int start, int end);
+
+
 int main(){
 	
 	cout << "This program counts the number of zeros" << endl;
 
-	int arr[] = {0,0,0,0,0};
+	int arr[] = {0,0,0,0,1,1};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	int option;
 	
-	cout << "Select mode: \n" << "1. Iterative\n" << "2. Recursive\n" << "Your choice: ";
+	cout << "Select mode: \n" << "1. Iterative\n" << "2. Recursive\n" << "3. Recursive(2)\n" << "Your choice: ";
 	cin >> option;
 	
 	cout << endl;
@@ -33,6 +37,9 @@ int main(){
 			break;
 		case 2:
 			fn_recursive(arr,size);
+			break;
+		case 3:
+			fn_recursivetwo(arr,size);
 			break;
 		default:
 			cout << "Invalid choice" << endl;
@@ -78,6 +85,35 @@ int count_zero(int arr[],int position, int size){
 	}
 	else
 		return 0;
+}
+
+void fn_recursivetwo(int arr[], int size){
+	
+	int count = 0;
+	
+	count = count_zerotwo(arr, 0, size-1);
+	
+	cout << "Number of zeros: " << count << endl;
+	
+}
+
+int count_zerotwo(int arr[],int start, int end){
+	
+
+	if(start == end){
+		if(arr[start] == 0)
+			return 1;
+		else
+			return 0;
+	}
+	
+	int mid = (start+end)/2;
+
+	if(arr[mid]==0)	
+		return mid-start+1+count_zerotwo(arr, mid+1, end);
+	else
+		return count_zerotwo(arr, start, mid);
+	
 }
 			
 	
